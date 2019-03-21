62b40bad36e7d5da0d51db64b4b53e483604c4d4
RaeanneVillanueva <raeanne1999@gmail.com>
2019-03-21 10:57:54 +0800

    update in view

--- src/controller/MainController.java
+++ src/controller/MainController.java
@@ -1,17 +1,20 @@
 package controller; 
 
 import java.awt.event.ActionEvent;
+
 import java.awt.event.ActionListener;
 
 import javax.swing.*;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 
+
 import view.*;
 
 public class MainController {
 	
 	private SeqMultiplicationGUI seqView;
+//private Register register;
 	
 	public MainController(SeqMultiplicationGUI sv) {
 		this.seqView = sv;
@@ -39,63 +42,18 @@ public class MainController {
 		return true;
 	}
 	
-	public String getMPrime(String input) {
-
-		StringBuilder mprime = new StringBuilder(input);
-		String flip = "";
-		String result = "";
-		char one = '1';
-		char zero = '0';
-		int index = 0;
-		
-		for(int k=input.length() - 1; k>=0; k--) {
-			if(input.charAt(k) != 1) {
-				flip = flip + input.charAt(k);
-			}
-		}
-		
-		for(int i=0; i<flip.length(); i++) {
-			if(flip.charAt(i) == one){
-				index = i;
-			}
-		}
-		
-		System.out.println(index);
-		
-				
-	/*	for(int j=0; j<flip.length(); j++) {
-			if(j != index) {
-				if(flip.length() == one) {
-					mprime.setCharAt(j, one);
-				}else {
-					mprime.setCharAt(j, zero);
-				}
-			}else {
-				
-			}
-		}*/
-		
-		
-		
-		return flip;
-	}
-	
 	class BtnLoadActionListener implements ActionListener{
 
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			
+		
 			if(!validInput()) {
 				JOptionPane.showMessageDialog(seqView, "Invalid Input!");
-				seqView.resetInput();
+			}else if(seqView.getInputM().equalsIgnoreCase("") || seqView.getInputQ().equalsIgnoreCase("")) {
+				JOptionPane.showMessageDialog(seqView, "Invalid!");
 			}else {
 				seqView.getTabbedPane().setSelectedIndex(1);
-				seqView.setOutputM(seqView.getInputM());
-				seqView.setOutputQ(seqView.getInputQ());
-				seqView.setOutputMPrime(getMPrime(seqView.getInputM()));
-				seqView.setOutputA("000000");	
 			}
-			
 		}
 	}
 	
