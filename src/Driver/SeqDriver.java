ce03b3d812f2e8d0817ea50025fa525315d4b2cc
John Andrew Ing <john_andrew_ing@dlsu.edu.ph>
2019-04-02 14:29:29 +0800

    update

--- src/Driver/SeqDriver.java
+++ src/Driver/SeqDriver.java
@@ -2,7 +2,7 @@ package Driver;
 import controller.MainController;
 import view.SeqMultiplicationGUI;
 
-public class Driver {
+public class SeqDriver {
 	public static void main(String args[]) {
 		SeqMultiplicationGUI frame = new SeqMultiplicationGUI();
 		MainController control = new MainController(frame);
